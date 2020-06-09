cfa8bb73c1d431131bf47d13ee94cfcdd67d8500
Chirag Jain <jain.chirag925@gmail.com>
2020-06-09 21:52:13 +0530

    Inject a formatter to drop exc info to avoid non json lines in logs

--- sentry_structlog_experiments/setup_logs.py
+++ sentry_structlog_experiments/setup_logs.py
@@ -6,10 +6,12 @@ import structlog
 # Potentially dangerous, using library internals, no suitable alternatives for now
 from structlog._frames import _find_first_app_frame_and_name
 from structlog.processors import _figure_out_exc_info
+from structlog.stdlib import ProcessorFormatter
 
 from .env_vars import PROJECT_ROOT, DJANGO_LOG_LEVEL
 
 
+# Custom Formatters
 class StructlogAwareMessageFormatter(logging.Formatter):
     class AttrMapItem(NamedTuple):
         record_attr_name: str
@@ -53,6 +55,39 @@ class StructlogAwareMessageFormatter(logging.Formatter):
         return super().format(record=record)
 
 
+class DropExcInfoFormatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
+        record.exc_text = None
+        record.exc_info = None
+        return super().format(record=record)
+
+
+class CustomProcessFormatter(ProcessorFormatter, DropExcInfoFormatter):
+    """
+    A necessary evil where we inject DropExcInfoFormatter in the MRO.
+    ProcessorFormatter.format -> DropExcInfoFormatter.format -> logging.Formatter.format
+
+    Because we keep exc_info till the end (see wrap_for_process_formatter method below), when a call to
+    logging.Formatter.format is made it appends duplicate non json traceback to the log message that can break your
+    json only logs. To avoid this problem, we inject DropExcInfoFormatter in the call chain so exc info can be dropped
+    from the record before formatting
+    """
+
+    @staticmethod
+    def wrap_for_process_formatter(logger: logging.Logger, name: str,
+                                   event_dict: Dict[str, Any]) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
+        """
+        Replacement for structlog.stdlib.ProcessorFormatter.wrap_for_formatter that passes on _exc_info as
+        exc_info to LogRecord.__init__
+        """
+        args, kwargs = ProcessorFormatter.wrap_for_formatter(logger=logger, name=name, event_dict=event_dict)
+        event_dict = args[0] if args else kwargs.get('event_dict')
+        if event_dict:
+            kwargs['exc_info'] = event_dict.pop('_exc_info', None)
+        return args, kwargs
+
+
+# Custom Processors
 def format_exc_info(logger: logging.Logger, name: str, event_dict: Dict[str, Any]) -> Dict[str, Any]:
     """
     Format exception info but also save it to another key to process in wrap_for_process_formatter
@@ -64,20 +99,6 @@ def format_exc_info(logger: logging.Logger, name: str, event_dict: Dict[str, Any
     return structlog.processors.format_exc_info(logger=logger, name=name, event_dict=event_dict)
 
 
-def wrap_for_process_formatter(logger: logging.Logger, name: str,
-                               event_dict: Dict[str, Any]) -> Tuple[Tuple[Any, ...], Dict[str, Any]]:
-    """
-    Replacement for structlog.stdlib.ProcessorFormatter.wrap_for_formatter that passes on _exc_info as
-    exc_info to LogRecord.__init__
-    """
-    args, kwargs = structlog.stdlib.ProcessorFormatter.wrap_for_formatter(logger=logger, name=name,
-                                                                          event_dict=event_dict)
-    event_dict = args[0] if args else kwargs.get('event_dict')
-    if event_dict:
-        kwargs['exc_info'] = event_dict.pop('_exc_info', None)
-    return args, kwargs
-
-
 def add_module_and_lineno(logger: logging.Logger, name: str, event_dict: Dict[str, Any]) -> Dict[str, Any]:
     # see https://github.com/hynek/structlog/issues/253 for a feature request to get this done better
     frame, module_str = _find_first_app_frame_and_name(additional_ignores=[__name__, 'logging'])
@@ -85,6 +106,7 @@ def add_module_and_lineno(logger: logging.Logger, name: str, event_dict: Dict[st
     return event_dict
 
 
+# Actual logging config
 FOREIGN_PRE_CHAIN_PROCESSORS = (  # For logs being emitted from logging.Logger but use structlog's ProcessFormatter
     structlog.processors.TimeStamper(fmt='iso'),
     structlog.stdlib.add_log_level,
@@ -103,17 +125,17 @@ LOGGING_CONFIG = {
             'format': '[%(asctime)s][%(levelname)s][%(module)s:%(lineno)d] %(message)s'
         },
         'json_formatter': {
-            '()': structlog.stdlib.ProcessorFormatter,
+            '()': CustomProcessFormatter,
             'processor': structlog.processors.JSONRenderer(),
             'foreign_pre_chain': FOREIGN_PRE_CHAIN_PROCESSORS,
         },
         'colored': {
-            '()': structlog.stdlib.ProcessorFormatter,
+            '()': CustomProcessFormatter,
             'processor': structlog.dev.ConsoleRenderer(colors=True),
             'foreign_pre_chain': FOREIGN_PRE_CHAIN_PROCESSORS,
         },
         'key_value': {
-            '()': structlog.stdlib.ProcessorFormatter,
+            '()': CustomProcessFormatter,
             'processor': structlog.processors.KeyValueRenderer(key_order=['timestamp', 'level', 'event', 'logger']),
             'foreign_pre_chain': FOREIGN_PRE_CHAIN_PROCESSORS,
         }
@@ -142,7 +164,7 @@ LOGGING_CONFIG = {
 }
 
 
-def configure():
+def configure(**structlog_configure_kwargs):
     structlog.configure(
         processors=[
             structlog.stdlib.filter_by_level,
@@ -153,10 +175,11 @@ def configure():
             format_exc_info,
             structlog.processors.UnicodeDecoder(),
             add_module_and_lineno,
-            wrap_for_process_formatter,
+            CustomProcessFormatter.wrap_for_process_formatter,
         ],
         context_class=structlog.threadlocal.wrap_dict(dict),
         logger_factory=structlog.stdlib.LoggerFactory(),
         wrapper_class=structlog.stdlib.BoundLogger,
         cache_logger_on_first_use=True,
+        **structlog_configure_kwargs
     )
